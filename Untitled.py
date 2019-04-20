673adcd1bf38be034488b4477d049b764bacbd52
hadirana12345 <49807779+hadirana12345@users.noreply.github.com>
2019-04-20 14:28:11 +0500

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[7]:
+
+
+username=input("Enter user name ")
+password=int(input("Enter password"))
+if (username=="areeb")and(password==123):
+    print("welcome sir")
+elif(username!="areeb"):
+    print("username is incorrect")
+else:
+    print("Invalid password")
+
